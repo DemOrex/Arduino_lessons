8b90f342dd16a3dc359f6da9c715dd5600a8fa14
DemOrex <67753633+DemOrex@users.noreply.github.com>
2023-01-11 10:36:40 +0300

    Update Les_1.ino

--- LESSONS/Les_1.ino
+++ LESSONS/Les_1.ino
@@ -1,12 +1,10 @@
 // мигаем светодиодом
 const int LED=10; // вывод для подключения светодиода 10 (D10)
-void setup()
-{
+void setup(){
 // Конфигурируем вывод подключения светодиода как выход (OUTPUT)
 pinMode(LED, OUTPUT);
 }
-void loop()
-{
+void loop(){
 // включаем светодиод, подавая на вывод 1 (HIGH)
 digitalWrite(LED,HIGH);
 // пауза 1 сек (1000 мс)
